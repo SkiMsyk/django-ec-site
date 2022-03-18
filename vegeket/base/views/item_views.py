49a0ea7ffd7ed552668598f0899a433619571558
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-03-18 21:38:21 +0900

    create index.html and related matters

---
+++ vegeket/base/views/item_views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from base.models import Item
+
+
+class IndexListView(ListView):
+    model = Item
+    template_name = 'pages/index.html'
+
+
