c767b33b7522cb4b74425e0bc395403d035e4b13
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-03-18 20:36:56 +0900

    fix gitignore

--- vegeket/base/admin.py
+++ vegeket/base/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from base.models import Item, Category, Tag
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+# modify admin sites
+class TagInline(admin.TabularInline):
+    model = Item.tags.through
+
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
+    exclude = ['tags']
+
+# show models bellow in /admin
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+
+# making Group model invisible
+admin.site.unregister(Group)
